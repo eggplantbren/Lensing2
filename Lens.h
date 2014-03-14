6c3ba5f1033f31a6e24a992b71150188ad4ced3a
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-03-14 22:04:16 +1300

    base class for lenses

---
+++ Lens.h
@@ -0,0 +1,29 @@
+#ifndef Lensing2_Lens_h
+#define Lensing2_Lens_h
+
+#include <ostream>
+
+namespace Lensing2
+{
+
+class Lens
+{
+	protected:
+
+
+	public:
+		// Deflection angle formula
+		virtual void alpha(double x, double y,
+					double& ax, double& ay) const = 0;
+
+		// MCMC related stuff
+		virtual void from_prior() = 0;
+		virtual double perturb() = 0;
+
+		virtual void print(std::ostream& out) const = 0;
+};
+
+} // namespace Lensing2
+
+#endif
+
