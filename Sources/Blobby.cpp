bd07ac2176addeea47ced2991b7da09405ea3026
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-05 20:04:57 +1300

    Empty methods

--- Sources/Blobby.cpp
+++ Sources/Blobby.cpp
@@ -1,2 +1,27 @@
 #include "Blobby.h"
+#include "RandomNumberGenerator.h"
+
+using namespace std;
+using namespace Lensing2;
+using namespace DNest3;
+
+double Blobby::evaluate(double x, double y) const
+{
+	return 0.;
+}
+
+void Blobby::from_prior()
+{
+
+}
+
+double Blobby::perturb()
+{
+	return 0.;
+}
+
+void Blobby::print(ostream& out) const
+{
+
+}
 
--- Sources/Blobby.h
+++ Sources/Blobby.h
@@ -8,10 +8,15 @@ namespace Lensing2
 
 class Blobby:public Source
 {
+	private:
 
 
-
-
+	public:
+		// Required methods
+		double evaluate(double x, double y) const;
+		void from_prior();
+		double perturb();
+		void print(std::ostream& out) const;
 };
 
 } // namespace Lensing2
