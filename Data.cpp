d95b07058429d37df0eabe535e26a6eec54a7f7f
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-14 10:47:24 -0700

    Empty data class

---
+++ Data.cpp
@@ -0,0 +1,14 @@
+#include "Data.h"
+
+using namespace Lensing2;
+
+Data::Data()
+{
+
+}
+
+void Data::load(const char* filename)
+{
+
+}
+
---
+++ Data.h
@@ -0,0 +1,25 @@
+#ifndef _Data_
+#define _Data_
+
+namespace Lensing2
+{
+
+class Data
+{
+	private:
+
+	public:
+		Data();
+		void load(const char* filename);
+
+	// Singleton
+	private:
+		static Data instance;
+	public:
+		static Data& get_instance() { return instance; }
+};
+
+} // namespace Lensing2
+
+#endif
+
