e65c12f0dd29927713c1c4e5986aa1df9e0549cb
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-14 11:19:08 -0700

    Reads metadata from file

--- Data.cpp
+++ Data.cpp
@@ -1,14 +1,38 @@
 #include "Data.h"
+#include <iostream>
+#include <fstream>
+#include <cmath>
 
+using namespace std;
 using namespace Lensing2;
+
+Data Data::instance;
 
 Data::Data()
 {
 
 }
 
-void Data::load(const char* filename)
+void Data::load(const char* metadata_file)
 {
+	// Read in the metadata
+	fstream fin(metadata_file, ios::in);
+	if(!fin)
+		cerr<<"# ERROR: couldn't open file "<<metadata_file<<"."<<endl;
+	fin>>ni>>nj;
+	fin>>x_min>>x_max>>y_min>>y_max;
+	fin.close();
 
+	// Make sure maximum > minimum
+	if(x_max <= x_min || y_max <= y_min)
+		cerr<<"# ERROR: strange input in "<<metadata_file<<"."<<endl;
+
+	// Compute pixel widths
+	dx = (x_max - x_min)/nj;
+	dy = (y_max - y_min)/ni;
+
+	// Check that pixels are square
+	if(abs(log(dx/dy)) >= 1E-3)
+		cerr<<"# ERROR: pixels aren't square."<<endl;
 }
 
--- Data.h
+++ Data.h
@@ -7,10 +7,21 @@ namespace Lensing2
 class Data
 {
 	private:
+		// Number of pixels
+		int ni, nj;
+
+		// Coordinates of image boundaries
+		double x_min, x_max, y_min, y_max;
+
+		// Pixel widths
+		double dx, dy;
 
 	public:
 		Data();
-		void load(const char* filename);
+		void load(const char* metadata_file);
+
+		int get_ni() const { return ni; }
+		int get_nj() const { return nj; }
 
 	// Singleton
 	private:
