7d9a18798045e63fb7e9310348fed46db72ba7ec
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-14 11:30:34 -0700

    Constants class

---
+++ Constants.cpp
@@ -0,0 +1,7 @@
+#include "Constants.h"
+
+using namespace Lensing2;
+
+// Number of rays per pixel per dimension
+const int Constants::nRays = 2;
+
---
+++ Constants.h
@@ -0,0 +1,16 @@
+#ifndef _Constants_
+#define _Constants_
+
+namespace Lensing2
+{
+
+class Constants
+{
+	public:
+		static const int nRays;
+};
+
+} // namespace Lensing2
+
+#endif
+
