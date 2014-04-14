1932a0979ba2c8ae663c400ee483e0fe88bd2e5f
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-14 11:59:20 -0700

    MyModel contains a source

--- MyModel.cpp
+++ MyModel.cpp
@@ -1,43 +1,34 @@
-/*
-* Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
-*
-* This file is part of DNest3.
-*
-* DNest3 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* DNest3 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with DNest3. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "MyModel.h"
+#include "Data.h"
+
 #include "RandomNumberGenerator.h"
 #include "Utils.h"
 #include <cmath>
 
 using namespace std;
+using namespace Lensing2;
 using namespace DNest3;
 
 MyModel::MyModel()
+:source(Data::get_instance().get_x_min(), Data::get_instance().get_x_max(),
+	Data::get_instance().get_y_min(), Data::get_instance().get_y_max(),
+	1E-3, 1E3)
 {
 
 }
 
 void MyModel::fromPrior()
 {
-
+	source.from_prior();
 }
 
 double MyModel::perturb()
 {
-	return 0.;
+	double logH = 0.;
+
+	logH += source.perturb();
+
+	return logH;
 }
 
 double MyModel::logLikelihood() const
@@ -47,7 +38,7 @@ double MyModel::logLikelihood() const
 
 void MyModel::print(std::ostream& out) const
 {
-
+	out<<' ';
 }
 
 string MyModel::description() const
--- MyModel.h
+++ MyModel.h
@@ -1,31 +1,17 @@
-/*
-* Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
-*
-* This file is part of DNest3.
-*
-* DNest3 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* DNest3 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with DNest3. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #ifndef _MyModel_
 #define _MyModel_
 
 #include "Model.h"
+#include "Sources/Blobby.h"
 #include <vector>
+
+namespace Lensing2
+{
 
 class MyModel:public DNest3::Model
 {
 	private:
+		Blobby source;
 
 	public:
 		MyModel();
@@ -45,6 +31,8 @@ class MyModel:public DNest3::Model
 		// Return string with column information
 		std::string description() const;
 };
+
+} // namespace Lensing2
 
 #endif
 
