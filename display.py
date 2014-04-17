e31924b1a45a87e9a2b3c894a5ffa9795a7a62d2
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-18 09:54:27 +1200

    SHow residuals

--- display.py
+++ display.py
@@ -1,12 +1,13 @@
 from pylab import *
 
 output = atleast_2d(loadtxt('posterior_sample.txt'))
+data = loadtxt('Data/test_image.txt')
 
 ion()
 hold(False)
 for i in xrange(0, output.shape[0]):
 	x = output[i, :]
-	imshow(x.reshape((100, 100)))
+	imshow(x.reshape((100, 100)) - data)
 	draw()
 
 ioff()
