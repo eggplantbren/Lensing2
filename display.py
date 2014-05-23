ef49598af0a6bc45c593b020b69ce7b01df01d36
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-05-23 13:23:36 +1200

    Show more stuff (model images, data)

--- display.py
+++ display.py
@@ -1,13 +1,30 @@
+"""
+A little script to load the output and plot the model images
+and the residuals.
+"""
 from pylab import *
 
 output = atleast_2d(loadtxt('posterior_sample.txt'))
 data = loadtxt('Data/test_image.txt')
 
+figure(figsize=(10, 6))
 ion()
 hold(False)
 for i in xrange(0, output.shape[0]):
 	x = output[i, 8:]
-	imshow(x.reshape((100, 100)) - data)
+	img = x.reshape((100, 100))
+
+	subplot(1,3,1)
+	imshow(img)
+	title('Model Image')
+
+	subplot(1,3,2)
+	imshow(data)
+	title('Data')
+
+	subplot(1,3,3)
+	imshow(img - data)
+	title('Residuals')
 	draw()
 
 ioff()
