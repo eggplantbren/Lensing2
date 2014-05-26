7a403485d2c78966fb18a015b64f1438b99d6a60
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-05-26 12:24:51 +1200

    Deflection angles moved into a function

--- Lenses/nie.py
+++ Lenses/nie.py
@@ -1,18 +1,23 @@
 from pylab import *
 
+b = 1.5
+q = 0.6
+rc = 0.
+
+def alpha(x, y):
+  psi = sqrt(q**2*(x**2 + rc**2) + y**2)
+  ax = b/sqrt(1. - q**2)*arctan(sqrt(1. - q**2)*x/(psi + rc))
+  ay = b/sqrt(1. - q**2)*arctanh(sqrt(1. - q**2)*y/(psi + q**2*rc))
+  return [ax, ay]
+
 x = linspace(-2., 2., 1001) + 1E-6
 y = linspace(-2., 2., 1001) + 1E-6
 [x, y] = meshgrid(x, y)
 y = y[::-1, :]
 h = x[0, 1] - x[0, 0]
 
-b = 2.
-q = 0.6
-rc = 0.
-
+[ax, ay] = alpha(x, y)
 psi = sqrt(q**2*(x**2 + rc**2) + y**2)
-ax = b/sqrt(1. - q**2)*arctan(sqrt(1. - q**2)*x/(psi + rc))
-ay = b/sqrt(1. - q**2)*arctanh(sqrt(1. - q**2)*y/(psi + q**2*rc))
 
 # Terms in divergence of alpha
 term1 = zeros(x.shape)
