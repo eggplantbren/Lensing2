b10bd3d8f578e08deda30dfe31085e15395962d3
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-05-28 11:57:29 +1200

    Copied and renamed NIE

---
+++ Lenses/SpottyNIE.h
@@ -0,0 +1,46 @@
+#ifndef Lensing2_SpottyNIE_h
+#define Lensing2_SpottyNIE_h
+
+#include <ostream>
+#include "Lens.h"
+
+namespace Lensing2
+{
+
+/*
+* A class for a single SpottyNIE.
+*/
+class SpottyNIE:public Lens
+{
+	private:
+		// Image dimensions
+		double x_min, x_max, y_min, y_max, scale;
+
+		// Strength, axis ratio, core radius
+		double b, q, rc;
+
+		// Position
+		double xc, yc;
+
+		// Orientation angle
+		double theta, cos_theta, sin_theta;
+
+		// External shear
+		double shear;
+		double theta_shear, cos_theta_shear, sin_theta_shear;
+
+	public:
+		// Constructor: pass in dimensions of image
+		SpottyNIE(double x_min, double x_max, double y_min, double y_max);
+
+		// Needed methods
+		void alpha(double x, double y, double& ax, double& ay) const;
+		void from_prior();
+		double perturb();
+		void print(std::ostream& out) const;
+};
+
+}
+
+#endif
+
