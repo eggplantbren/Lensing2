ec3d1b4408bae5e6634c47a98bb27028b6b804c6
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-05-30 11:39:07 +1200

    Removed obsolete function

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@ using namespace std;
 using namespace DNest3;
 using namespace Lensing2;
 
-void test1();
-
 int main(int argc, char** argv)
 {
 	// Load some "data"
@@ -25,39 +23,4 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-void test1()
-{
-	RandomNumberGenerator::initialise_instance();
-	RandomNumberGenerator::get_instance().set_seed(2);
-
-	Blobby b(-1., 1., -1., 1., 1E-3, 1E3);
-	b.from_prior();
-	Blobby b2 = b;
-
-	int steps = 10000;
-	int skip = 10;
-
-	fstream fout("output.txt", ios::out);
-	// Explore the prior for Blobbys
-	for(int i=0; i<steps; i++)
-	{
-		b2 = b;
-		double logH = b2.perturb();
-		if(randomU() <= exp(logH))
-			b = b2;
-
-		if(i%skip == 0)
-		{
-			for(double y = 0.99; y >= -0.99; y -= 0.01)
-			{
-				for(double x = -0.99; x <= 0.99; x += 0.01)
-					fout<<b.evaluate(x, y)<<' ';
-			}
-			fout<<endl;
-			cout<<i<<endl;
-		}
-	}
-
-	fout.close();
-}
 
