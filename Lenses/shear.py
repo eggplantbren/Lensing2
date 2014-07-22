b17199ffad1579a19c9cd63cc1576410ebf811c9
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-22 17:33:58 +1200

    Think I've got it

--- Lenses/shear.py
+++ Lenses/shear.py
@@ -1,6 +1,6 @@
 from pylab import *
 
-def alpha(x, y, shear=0.1, theta_shear=0.):
+def alpha(x, y, shear=1, theta_shear=0.):
 	cos_theta_shear = cos(theta_shear)
 	sin_theta_shear = sin(theta_shear)
 
@@ -9,8 +9,8 @@ def alpha(x, y, shear=0.1, theta_shear=0.):
 	yy = -x*sin_theta_shear + y*cos_theta_shear;
 
 	# Calculate external shear
-	alphax =  shear*xx;
-	alphay = -shear*yy;
+	alphax = -shear*xx;
+	alphay = shear*yy;
 
 	# Add external shear
 	ax = alphax*cos_theta_shear - alphay*sin_theta_shear;
@@ -19,9 +19,20 @@ def alpha(x, y, shear=0.1, theta_shear=0.):
 	return [ax, ay]
 
 
+# Point mass going in a circle
+def alpha_point_mass(x, y, theta=0.):
+	xc = 100*cos(theta)
+	yc = 100*sin(theta)
+	rsq = (x - xc)**2 + (y - yc)**2
+	ax = (x - xc)/rsq
+	ay = (y - yc)/rsq
 
-x = -0.5
-y = 0.7
+	return [ax, ay]
+
+
+
+x = 0.01
+y = 0.
 
 h = 1E-3
 
@@ -42,6 +53,17 @@ for theta_shear in linspace(0, pi, 101):
 	hold(True)
 	axis('equal')
 	title(theta_shear)
+
+	draw()
+
+# What does a distant point mass do?
+for theta in linspace(0, 2*pi, 101):
+	[ax, ay] = alpha_point_mass(x, y, theta=theta)
+
+	plot(ax, ay, 'ro')
+	hold(True)
+	axis('equal')
+	title(theta)
 
 	draw()
 
