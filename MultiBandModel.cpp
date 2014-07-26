d211d39d5312541b59d9c453736f10acbb38635f
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-26 20:46:40 +1200

    Constructor and fromPrior

--- MultiBandModel.cpp
+++ MultiBandModel.cpp
@@ -10,14 +10,21 @@ using namespace Lensing2;
 using namespace DNest3;
 
 MultiBandModel::MultiBandModel()
-:source(MultiBandData::get_instance().get_images().size())
+:source(MultiBandData::get_instance().get_images().size(),
+	Blobby(Data::get_instance().get_x_min(), Data::get_instance().get_x_max(),
+	Data::get_instance().get_y_min(), Data::get_instance().get_y_max(),
+	1E-3, 1E3))
+,lens(Data::get_instance().get_x_min(), Data::get_instance().get_x_max(),
+	Data::get_instance().get_y_min(), Data::get_instance().get_y_max())
 {
 
 }
 
 void MultiBandModel::fromPrior()
 {
-
+	for(size_t i=0; i<source.size(); i++)
+		source[i].from_prior();
+	lens.from_prior();
 }
 
 double MultiBandModel::perturb()
