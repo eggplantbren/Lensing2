86739d716e2cc9fbe4834359139eddde946da7e2
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-26 20:18:04 +1200

    Load method

--- MultiBandData.cpp
+++ MultiBandData.cpp
@@ -10,7 +10,13 @@ MultiBandData::MultiBandData()
 
 }
 
-
+void MultiBandData::load(const char* metadata_file, const char* image_file,
+				const char* sigma_file, const char* psf_file)
+{
+	Data data;
+	data.load(metadata_file, image_file, sigma_file, psf_file);
+	images.push_back(data);
+}
 
 } // namespace Lensing2
 
--- MultiBandData.h
+++ MultiBandData.h
@@ -10,10 +10,16 @@ namespace Lensing2
 class MultiBandData
 {
 	private:
-		std::vector<Data> bands;
+		std::vector<Data> images;
 
 	public:
 		MultiBandData();
+
+		// Same load function as regular data. Appends to the 'images'
+		// vector
+		void load(const char* metadata_file, const char* image_file,
+				const char* sigma_file, const char* psf_file);
+
 
 		static MultiBandData instance;
 };
