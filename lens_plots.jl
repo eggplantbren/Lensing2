c81776d915b17bfbc3ccfe36a0c5e22d31896011
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-07-19 09:38:41 +1200

    Maybe just areas?

--- lens_plots.jl
+++ lens_plots.jl
@@ -29,24 +29,27 @@ x = linspace(-10,  10, 3001)
 y = linspace( 10, -10, 3001)
 mag = magnification_image(posterior_sample[which, :], x, y)
 
-# Use matplotlib's contour function to get the critical curve
-# http://stackoverflow.com/questions/5666056/matplotlib-extracting-data-from-contour-lines
-contour = plt.contour(x, y, mag, [5.5, 5.51])
-plt.clf()
-plt.imshow(mag, interpolation="nearest", extent=[-10, 10, -10, 10],
-					vmin=-7.0, vmax=7.0, cmap="coolwarm")
-contour = contour[:collections][:1]
-contour = contour[:get_paths]()[1][:vertices]
-plt.plot(contour[:,1], contour[:,2], "k", linewidth=2)
-plt.show()
+## Use matplotlib's contour function to get the critical curve
+## http://stackoverflow.com/questions/5666056/matplotlib-extracting-data-from-contour-lines
+#contour = plt.contour(x, y, mag, [5.5, 5.51])
+#plt.clf()
+#plt.imshow(mag, interpolation="nearest", extent=[-10, 10, -10, 10],
+#					vmin=-7.0, vmax=7.0, cmap="coolwarm")
+#contour = contour[:collections][:1]
+#contour = contour[:get_paths]()[1][:vertices]
+#plt.plot(contour[:,1], contour[:,2], "k", linewidth=2)
+#plt.show()
 
-# Fire the contour through to the source plane
-xs = contour[:,1]
-ys = contour[:,2]
-for(i in 1:size(xs)[1])
-	(xs[i], ys[i]) = fire_ray(posterior_sample[which, :],
-									contour[i, 1], contour[i, 2])
-end
+## Fire the contour through to the source plane
+#xs = contour[:,1]
+#ys = contour[:,2]
+#for(i in 1:size(xs)[1])
+#	(xs[i], ys[i]) = fire_ray(posterior_sample[which, :],
+#									contour[i, 1], contour[i, 2])
+#end
+
+plt.imshow(mag .> 5., interpolation="nearest", cmap="gray")
+plt.show()
 
 plt.figure(figsize=(8, 8))
 plt.plot(xs, ys, "k-")
