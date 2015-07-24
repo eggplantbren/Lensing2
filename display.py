Fixed mass units: all in terms of critical density. No simple b^2

display.py
@@ -119,7 +119,7 @@ show()
 figure(3)
 rc("font", size=16, family="serif", serif="Computer Sans")
 rc("text", usetex=True)
-plot(output[:,2]**2, output[:,39:48].sum(axis=1), 'bo', markersize=5,\
+plot(output[:,2]**2*pi, output[:,39:48].sum(axis=1), 'bo', markersize=5,\
                        alpha=0.2)
 xlabel('SIE mass (within critical ellipse)')
 ylabel('Total substructure mass')