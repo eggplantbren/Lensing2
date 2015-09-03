c76fcce67207ac33b11cb22938442c327f88aaf8
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-03 10:54:39 +0100

    Masking

--- MyModel.cpp
+++ MyModel.cpp
@@ -135,27 +135,35 @@ void MyModel::shoot_rays()
 {
 	const vector< vector<long double> >& x = Data::get_instance().get_x_rays();
 	const vector< vector<long double> >& y = Data::get_instance().get_y_rays();
+	const vector< vector<double> >& sigma = Data::get_instance().get_sigma();
 
 	double ax, ay;
 	for(size_t i=0; i<xs.size(); i++)
 	{
 		for(size_t j=0; j<xs[i].size(); j++)
 		{
-			lens.alpha(x[i][j], y[i][j], ax, ay);
-			xs[i][j] = x[i][j] - ax;
-			ys[i][j] = y[i][j] - ay;
+			// Extremely high sigma values are "masked"
+			if(sigma[i][j] < 1E100)
+			{
+				lens.alpha(x[i][j], y[i][j], ax, ay);
+				xs[i][j] = x[i][j] - ax;
+				ys[i][j] = y[i][j] - ay;
+			}
 		}
 	}
 }
 
 void MyModel::calculate_surface_brightness()
 {
+	const vector< vector<double> >& sigma = Data::get_instance().get_sigma();
+
 	for(size_t i=0; i<xs.size(); i++)
 	{
 		for(size_t j=0; j<xs[i].size(); j++)
 		{
-			surface_brightness[i][j] = source.evaluate(xs[i][j],
-								ys[i][j]);
+			// Extremely high sigma values are "masked"
+			if(sigma[i][j] < 1E100)
+				surface_brightness[i][j] = source.evaluate(xs[i][j], ys[i][j]);
 		}
 	}
 
