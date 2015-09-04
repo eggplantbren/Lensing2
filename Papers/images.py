35a1738e7e8c3a4bfa93a21168854924ac8bc981
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-04 17:11:59 +0100

    Show the substructures

--- Papers/images.py
+++ Papers/images.py
@@ -34,7 +34,29 @@ savefig("image1.pdf", bbox_inches="tight")
 show()
 
 img = loadtxt("../Data/harder_image.txt")
+truth = loadtxt("../Data/harder_truth.txt")
+metadata = loadtxt("../Data/mock_metadata.txt")
+
+# Convert x and y to pixel coordinates for overplotting
+dx = (metadata[3] - metadata[2])/metadata[1]
+dy = (metadata[5] - metadata[4])/metadata[0]
+x_substructures = truth[19:29]
+y_substructures = truth[29:39]
+x_substructures = x_substructures[x_substructures != 0.]
+y_substructures = y_substructures[y_substructures != 0.]
+x_substructures = (x_substructures - metadata[2])/dx - 0.5
+y_substructures = (metadata[5] - y_substructures)/dy - 0.5
+x_nie = (truth[5] - metadata[2])/dx - 0.5
+y_nie = (metadata[5] - truth[6])/dy - 0.5
+
 imshow(img, interpolation="nearest", cmap="Oranges")
+hold(True)
+# Plot center of NIE
+plot(x_nie, y_nie, 'wo', markersize=15)
+# Substructures
+plot(x_substructures, y_substructures, 'g*', markersize=15)
+xlim([-0.5, metadata[1] - 0.5])
+ylim([metadata[0] - 0.5, -0.5])
 gca().set_xticklabels("")
 gca().set_yticklabels("")
 title("Simulated Dataset")
