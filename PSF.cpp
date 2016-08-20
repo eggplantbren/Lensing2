40a9efa44d28f00e083f11151a8ca15dcfc1c016
Brendon Brewer <brendon.brewer@gmail.com>
2016-08-20 13:24:21 +1200

    ported

--- PSF.cpp
+++ PSF.cpp
@@ -10,18 +10,18 @@ using namespace arma;
 
 PSF::PSF(int size)
 :size(size)
-,pixels(size, vector<double>(size, 0.))
+,pixels(size, size)
 ,fft_ready(false)
 {
 	assert(size%2 == 1);
-	pixels[size/2][size/2] = 1.;
+	pixels(size/2, size/2) = 1.;
 }
 
 void PSF::set_size(int new_size)
 {
 	size = new_size;
-	pixels.assign(size, vector<double>(size, 0.));
-	pixels[size/2][size/2] = 1.;
+	pixels = arma::mat(size, size);
+	pixels(size/2, size/2) = 1.;
 }
 
 void PSF::load(const char* filename)
@@ -34,7 +34,7 @@ void PSF::load(const char* filename)
 	}
 	for(int i=0; i<size; i++)
 		for(int j=0; j<size; j++)
-			fin>>pixels[i][j];
+			fin>>pixels(i, j);
 	fin.close();
 	normalise();
 }
@@ -44,11 +44,11 @@ void PSF::normalise()
 	double sum = 0.;
 	for(int i=0; i<size; i++)
 		for(int j=0; j<size; j++)
-			sum += pixels[i][j];
+			sum += pixels(i, j);
 
 	for(int i=0; i<size; i++)
 		for(int j=0; j<size; j++)
-			pixels[i][j] /= sum;
+			pixels(i, j) /= sum;
 }
 
 void PSF::calculate_fft(int Ni, int Nj)
@@ -57,8 +57,8 @@ void PSF::calculate_fft(int Ni, int Nj)
 	mat psf(Ni, Nj);
 	psf.zeros();
 
-	int ni = pixels.size();
-	int nj = pixels[0].size();
+	int ni = pixels.n_rows;
+	int nj = pixels.n_cols;
 
 	int m, n;
 	for(int i=0; i<ni; i++)
@@ -67,7 +67,7 @@ void PSF::calculate_fft(int Ni, int Nj)
 		for(int j=0; j<nj; j++)
 		{
 			n = mod(j - nj/2, Nj);
-			psf(m, n) = pixels[i][j];
+			psf(m, n) = pixels(i, j);
 		}
 	}
 
@@ -75,102 +75,28 @@ void PSF::calculate_fft(int Ni, int Nj)
 	fft_ready = true;
 }
 
-void PSF::blur_image(vector< vector<double> >& img) const
-{
-	// Make result image. Assume img is rectangular...
-	vector< vector<double> > result(img.size(),
-					vector<double>(img[0].size(), 0.));
 
-	int h = size/2;
-	int ii, jj;
-	int M = static_cast<int>(img.size());
-	int N = static_cast<int>(img[0].size());
-
-	for(int i=0; i<M; i++)
-	{
-		for(int j=0; j<N; j++)
-		{
-			if(img[i][j] != 0.)
-			{
-				for(int m=0; m<size; m++)
-				{
-					ii = i + m - h;
-					for(int n=0; n<size; n++)
-					{
-						jj = j + n - h;
-						if(ii >= 0 && ii < M &&
-							jj >= 0 && jj < N)
-							result[ii][jj] +=
-							img[i][j]*pixels[m][n];
-					}
-				}
-			}
-		}
-	}
-
-	img = result;
-}
-
-void PSF::blur_image2(vector< vector<double> >& img) const
+void PSF::blur_image2(mat& img) const
 {
 	if(!fft_ready)
 		cerr<<"# Blurring failed."<<endl;
 
-	// Copy the image into an Armadillo matrix
-	mat A(img.size(), img[0].size());
-	for(size_t i=0; i<img.size(); i++)	
-		for(size_t j=0; j<img[0].size(); j++)
-			A(i, j) = img[i][j];
-
+    // A = img
 	// Do the fft of it
-	cx_mat B = fft2(A);
+	cx_mat B = fft2(img);
 
 	// Multiply the two ffts
-	for(size_t i=0; i<img.size(); i++)
-		for(size_t j=0; j<img[0].size(); j++)
+	for(size_t j=0; j<img.n_cols; ++j)
+        for(size_t i=0; i<img.n_rows; ++i)
 			B(i, j) *= fft_of_psf(i, j);
 
 	// Do the inverse fft
 	B = ifft2(B);
 
 	// Put back in img
-	for(size_t i=0; i<img.size(); i++)
-		for(size_t j=0; j<img[0].size(); j++)
-			img[i][j] = real(B(i, j));
-
-}
-
-
-void PSF::test()
-{
-	PSF psf(5);
-	psf.load("Data/test_psf.txt");
-	psf.calculate_fft(20, 20);
-
-	// Point image
-	vector< vector<double> > pixels(20, vector<double>(20, 0.));
-	pixels[10][10] = 1.;
-
-	psf.blur_image(pixels);
-	// Print image and reset it to zero
-	for(size_t i=0; i<pixels.size(); i++)
-	{
-		for(size_t j=0; j<pixels.size(); j++)
-		{
-			cout<<pixels[i][j]<<' ';
-			pixels[i][j] = 0.;
-		}
-	}
-	cout<<endl;
-
-	// Do it again with ffts
-	pixels[10][10] = 1.;
-
-	psf.blur_image2(pixels);
-	for(size_t i=0; i<pixels.size(); i++)
-		for(size_t j=0; j<pixels.size(); j++)
-			cout<<pixels[i][j]<<' ';
-	cout<<endl;
+	for(size_t j=0; j<img.n_cols; ++j)
+        for(size_t i=0; i<img.n_rows; ++i)
+			img(i, j) = real(B(i, j));
 }
 
 
--- PSF.h
+++ PSF.h
@@ -8,7 +8,7 @@ class PSF
 {
 	private:
 		int size;
-		std::vector< std::vector<double> > pixels;
+		arma::mat pixels;
 
 		// FFT of the PSF
 		arma::cx_mat fft_of_psf;
@@ -17,17 +17,17 @@ class PSF
 	public:
 		PSF(int size);
 		// Uses loopy method
-		void blur_image(std::vector< std::vector<double> >& img) const;
+//		void blur_image(std::vector< std::vector<double> >& img) const;
 		// Uses fft method
 		void blur_image2
-			(std::vector< std::vector<double> >& img) const;
+			(arma::mat& img) const;
 		void normalise();
 		void calculate_fft(int Ni, int Nj);
 		void load(const char* filename);
 		void set_size(int new_size);
 
 		// Getter for pixels
-		const std::vector< std::vector<double> >& get_pixels() const
+		const arma::mat& get_pixels() const
 		{ return pixels; }
 
 		// Unit test
