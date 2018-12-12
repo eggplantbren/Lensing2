13a8227fad57c9d6e3b277d30d2e723f22f61382
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-12-13 10:20:14 +1300

    No stretch

--- src/source_display.py
+++ src/source_display.py
@@ -91,7 +91,7 @@ for i in range(posterior_sample.shape[0]):
 #    plt.imshow(img, origin="lower", extent=[x_min, x_max, y_min, y_max])
 #    plt.show()
 
-plt.imshow((tot/posterior_sample.shape[0])**0.5,
+plt.imshow(tot/posterior_sample.shape[0],
             origin="lower", extent=[x_min, x_max, y_min, y_max])
 plt.title("Posterior mean source")
 plt.show()
